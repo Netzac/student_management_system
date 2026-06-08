1c4ac533ae1f26521a189b739f41c45c129e4ed4
NetZac <WGsystems777@gmail.com>
2026-06-08 00:34:20 +0000

    modiy student account ui

--- search/tests.py
+++ search/tests.py
@@ -1,3 +1,55 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.utils import timezone
 
-# Create your tests here.
+from bookstore.models import Book, Category
+
+
+class SearchViewTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.category = Category.objects.create(name="Science")
+        cls.other_category = Category.objects.create(name="History")
+        cls.science_book = Book.objects.create(
+            category=cls.category,
+            isbn="SCI-001",
+            title="Modern Physics",
+            author="Ada Newton",
+            publisher="Academy Press",
+            date_published=timezone.now(),
+        )
+        cls.history_book = Book.objects.create(
+            category=cls.other_category,
+            isbn="HIS-001",
+            title="Ancient Worlds",
+            author="Mary Curie",
+            publisher="Scholars Press",
+            date_published=timezone.now(),
+        )
+
+    def test_search_matches_book_title(self):
+        response = self.client.get(reverse("search:search"), {"q": "physics"})
+
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerySetEqual(
+            response.context["book"].object_list,
+            [self.science_book],
+        )
+
+    def test_search_matches_category_name(self):
+        response = self.client.get(reverse("search:search"), {"q": "science"})
+
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerySetEqual(
+            response.context["book"].object_list,
+            [self.science_book],
+        )
+
+    def test_search_matches_author_text_field(self):
+        response = self.client.get(reverse("search:search"), {"q": "curie"})
+
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerySetEqual(
+            response.context["book"].object_list,
+            [self.history_book],
+        )
--- search/views.py
+++ search/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
-from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.core.paginator import Paginator
 from django.db.models import Q
-from bookstore.models import Book, Category, Author
+from bookstore.models import Book
 
 def search(request):
 	search = request.GET.get('q')
-	books = Book.objects.all()
+	books = Book.objects.all().order_by("id")
 	if search:
 		books = books.filter(
-			Q(title__icontains=search)|Q(category__name__icontains=search)|Q(author__name__icontains=search)
+			Q(title__icontains=search)|Q(category__name__icontains=search)|Q(author__icontains=search)
 
 		)
 
